6cef895362b6225469d60e4c2b388783411acbf4
George <gchriscarter@gmail.com>
2021-02-22 15:54:02 -0500

    added coding_language instance variable

--- app/controllers/api/welcomes_controller.rb
+++ app/controllers/api/welcomes_controller.rb
@@ -1,5 +1,11 @@
 class Api::WelcomesController < ApplicationController
   def hello
+    @random = Random.new.rand(100)
     render "hello.json.jb"
   end
+
+  def about 
+    @coding = "ruby"
+    render "about.json.jb"
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   namespace :api do
     get "/welcome_path" => "welcomes#hello"
+    get "/about_path" => "welcomes#about"
   end
 end
